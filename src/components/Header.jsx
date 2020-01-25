fa79a92970771f2c85b0d6a4c5c66a84ec414f4c
Jimmy <bruno_pier@hotmail.com>
2020-01-25 12:42:58 -0300

    Adding Footer.js file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
 
     return(
-            <h1 class="encabezado">Encabezado</h1>
+            <h1 id="encabezado" className="encabezado">Encabezado</h1>
 
     );
 }
