b118e4e3ae1337fda0e6d9427a2d591aca188613
Jimmy <bruno_pier@hotmail.com>
2020-01-25 14:54:00 -0300

    Adding button&event&const - Producto.js

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -2,13 +2,24 @@ import React from 'react';
 
 const Producto = ({producto}) => {
 
-    const {nombre, precio, id} = producto
+    const {nombre, precio, id} = producto;
+
+    //Agrgar producto
+    const selecionarProducto = () => {
+        console.log('comprando...')
+    };
 
     return (
 
         <div>
             <h2>{nombre}</h2>
             <p>${precio}</p>
+            <button
+                type="button"
+                onClick={() => selecionarProducto()}
+                >
+                    Comprar
+            </button>
         </div>
 
     );
