95b7f484fd345e08bd207b1f87fad28fc36c073b
Jimmy <bruno_pier@hotmail.com>
2020-01-25 14:10:30 -0300

    Adding producto&.map&import producto - App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Producto from './components/Producto';
 
 
 function App() {
 
+  //Lista de productos - arr con obj
+  const [productos, guardarProductos] = useState([
+    {id: 1, nombre: 'Camisa ReactJs', precio: 50},
+    {id: 2, nombre: 'Camisa Redux', precio: 40},
+    {id: 3, nombre: 'Camisa JS', precio: 30},
+    {id: 4, nombre: 'Camisa Angular', precio: 20},
+
+  ]);
+
+
+
   const fecha = new Date().getFullYear();
   return (
       <Fragment>
+
         <Header
           titulo='Tienda Virtual'
         />
+
+        <h1>Lista de productos</h1>
+        {productos.map(producto => (
+          <Producto />
+        ))}
+
         <Footer
         fecha={fecha}
         />
-
 
       </Fragment>
   );
