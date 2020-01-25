7efa2a861ee982ac09ba901cbf0c1908da607a0c
Jimmy <bruno_pier@hotmail.com>
2020-01-25 12:41:48 -0300

    class&className test - Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,8 @@ import React from 'react';
 
 function Header() {
 
-    const edad = 18;
-
-    let mensaje;
-    if(edad >= 18){
-        mensaje = 'Eres mayor de edad'
-    }else {
-        mensaje = 'Eres menor de edad'
-    }
-
     return(
-            <h1>{mensaje}</h1>
+            <h1 class="encabezado">Encabezado</h1>
 
     );
 }
