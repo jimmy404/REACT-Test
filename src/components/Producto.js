b475c30a1272f16401097fec8eebd9dcbdc8ba8b
Jimmy <bruno_pier@hotmail.com>
2020-01-25 14:16:55 -0300

    Adding const&div with code to show data - Producto.js

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const Producto = () => {
-    return ( <h1>desde Producto</h1> );
+const Producto = ({producto}) => {
+
+    const {nombre, precio, id} = producto
+
+    return (
+
+        <div>
+            <h2>{nombre}</h2>
+            <p>${precio}</p>
+        </div>
+
+    );
 }
 
 export default Producto;
